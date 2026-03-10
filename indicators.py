ffa455652673b2de6ddb907fceeadafd5f7109f0
ProgSaif <isaifulone@gmail.com>
2026-03-10 13:24:39 +0600

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,27 +1,26 @@
 import pandas as pd
 
-def calculate_rsi(df, period=14):
+
+def rsi(df, period=14):
+
     delta = df["close"].diff()
 
     gain = (delta.where(delta > 0, 0)).rolling(period).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(period).mean()
 
     rs = gain / loss
-    rsi = 100 - (100 / (1 + rs))
+    df["rsi"] = 100 - (100 / (1 + rs))
 
-    df["RSI"] = rsi
     return df
 
 
-def calculate_ema(df, period):
-    return df["close"].ewm(span=period, adjust=False).mean()
+def ema(series, period):
+
+    return series.ewm(span=period, adjust=False).mean()
 
 
 def volume_spike(df):
 
     avg = df["volume"].rolling(20).mean()
 
-    if df["volume"].iloc[-1] > avg.iloc[-1] * 1.5:
-        return True
-
-    return False
+    return df["volume"].iloc[-1] > avg.iloc[-1] * 1.5
