7c408ee131508757934e92f667b48eb8ca8d6ace
ProgSaif <isaifulone@gmail.com>
2026-03-10 13:25:26 +0600

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,22 +4,18 @@ from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 def send_signal(symbol, signal):
 
-    text = f"""
+    text=f"""
 🚀 {symbol} SIGNAL
 
 Direction: {signal['direction']}
-Entry: {signal['entry']}
+Entry: {round(signal['entry'],4)}
 
 Confidence: {signal['score']}%
-
-#CryptoSignal
 """
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    url=f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
-    data = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text
-    }
-
-    requests.post(url, data=data)
+    requests.post(url,data={
+        "chat_id":TELEGRAM_CHAT_ID,
+        "text":text
+    })
