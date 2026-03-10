0ff6b917b3d1495c84b1ba0b91031fa8433d2ac5
ProgSaif <isaifulone@gmail.com>
2026-03-10 13:25:11 +0600

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,11 +1,10 @@
-import pandas as pd
-from indicators import calculate_rsi, calculate_ema, volume_spike
+from indicators import rsi, ema, volume_spike
 
 
 def detect_trend(df):
 
-    ema50 = calculate_ema(df, 50)
-    ema200 = calculate_ema(df, 200)
+    ema50 = ema(df["close"],50)
+    ema200 = ema(df["close"],200)
 
     if ema50.iloc[-1] > ema200.iloc[-1]:
         return "up"
@@ -13,23 +12,23 @@ def detect_trend(df):
     return "down"
 
 
-def generate_signal(df_5m, df_1h):
+def generate_signal(df5, df1):
 
-    trend = detect_trend(df_1h)
+    trend = detect_trend(df1)
 
-    df_5m = calculate_rsi(df_5m)
+    df5 = rsi(df5)
 
-    ema9 = calculate_ema(df_5m, 9)
-    ema21 = calculate_ema(df_5m, 21)
+    ema9 = ema(df5["close"],9)
+    ema21 = ema(df5["close"],21)
 
-    rsi = df_5m["RSI"].iloc[-1]
+    rsi_val = df5["rsi"].iloc[-1]
 
-    vol_spike = volume_spike(df_5m)
+    vol = volume_spike(df5)
 
-    price_now = df_5m["close"].iloc[-1]
-    price_prev = df_5m["close"].iloc[-2]
+    price_now = df5["close"].iloc[-1]
+    price_prev = df5["close"].iloc[-2]
 
-    change = (price_now - price_prev) / price_prev * 100
+    change = (price_now-price_prev)/price_prev*100
 
     score = 0
 
@@ -39,22 +38,19 @@ def generate_signal(df_5m, df_1h):
     if ema9.iloc[-1] > ema21.iloc[-1]:
         score += 25
 
-    if vol_spike:
+    if vol:
         score += 20
 
-    if 40 < rsi < 70:
+    if 40 < rsi_val < 70:
         score += 15
 
     if change > 0.3:
         score += 10
 
     if score >= 60:
+        return {"direction":"LONG","entry":price_now,"score":score}
 
-        return {
-            "direction": "LONG",
-            "entry": round(price_now,4),
-            "score": score
-        }
+    score = 0
 
     if trend == "down":
         score += 30
@@ -62,21 +58,16 @@ def generate_signal(df_5m, df_1h):
     if ema9.iloc[-1] < ema21.iloc[-1]:
         score += 25
 
-    if vol_spike:
+    if vol:
         score += 20
 
-    if 30 < rsi < 60:
+    if 30 < rsi_val < 60:
         score += 15
 
     if change < -0.3:
         score += 10
 
     if score >= 60:
-
-        return {
-            "direction": "SHORT",
-            "entry": round(price_now,4),
-            "score": score
-        }
+        return {"direction":"SHORT","entry":price_now,"score":score}
 
     return None
